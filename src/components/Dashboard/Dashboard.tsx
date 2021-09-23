fb387e31016161feefda220d09d5e0571703f5cd
muhamad.zolfaghari <you@example.com>
2021-09-23 20:11:41 +0330

    Dark mode applied.

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -11,7 +11,8 @@ import food1Image from '../../images/foods/food1.png';
 import food2Image from '../../images/foods/food2.png';
 import CardHeader from '@mui/material/CardHeader';
 import Container from '@mui/material/Container';
-import { CardContent } from '@mui/material';
+import { Button, CardContent } from '@mui/material';
+import Box from '@mui/material/Box';
 
 const Dashboard = () => {
   return (
@@ -21,22 +22,57 @@ const Dashboard = () => {
         <Grid container spacing={5} className={classes.grid}>
           <Grid xs item>
             <Paper className={classes.paper}>
-              <Typography variant={'h4'}>Summary</Typography>
-              <Typography variant={'h4'}>
-                <FitnessCenterOutlinedIcon fontSize={'large'} /> Today Workout
+              <Typography variant={'h4'} gutterBottom>
+                Summary
               </Typography>
+              <Button variant={'outlined'} color={'primary'} fullWidth>
+                <Box display={'flex'}>
+                  <FitnessCenterOutlinedIcon fontSize={'large'} />
+                  <Typography variant={'h4'}>Today Workout</Typography>
+                </Box>
+              </Button>
+              <Card variant={'outlined'}>
+                <CardHeader
+                  title={'Full body details'}
+                  subheader={'See the health and information about body'}
+                />
+                <CardContent>
+                  The recipe of this food include fresh vegetable, some cheese
+                  and eggs, that mixed up with vegetable oil.
+                </CardContent>
+              </Card>
             </Paper>
           </Grid>
           <Grid xs item>
             <Paper className={classes.paper}>
-              <Typography variant={'h4'}>Recommendation Foods</Typography>
-              <Card>
+              <Typography variant={'h4'} gutterBottom>
+                Recommendation Foods
+              </Typography>
+              <Card variant={'outlined'}>
+                <CardHeader
+                  title={'Vegetarian Dish'}
+                  subheader={'The healthy food for have a healthy body'}
+                  avatar={
+                    <Avatar src={food1Image} sx={{ width: 80, height: 80 }} />
+                  }
+                />
                 <CardContent>
-                  <Avatar src={food1Image} />
+                  The recipe of this food include fresh vegetable, some cheese
+                  and eggs, that mixed up with vegetable oil.
                 </CardContent>
               </Card>
-              <Card>
-                <CardHeader avatar={<Avatar src={food2Image} />} />
+              <Card sx={{ marginTop: '1rem' }} variant={'outlined'}>
+                <CardHeader
+                  title={'Fish Meat'}
+                  subheader={'Superb meal to get protein'}
+                  avatar={
+                    <Avatar src={food2Image} sx={{ width: 80, height: 80 }} />
+                  }
+                />
+                <CardContent>
+                  This meal include source of protein and the best chose for
+                  whose want to get in shape after doing exercise.
+                </CardContent>
               </Card>
             </Paper>
           </Grid>
