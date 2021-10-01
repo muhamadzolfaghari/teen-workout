7baa042d78a91b97d8c91a3eadb2f820c17181de
muhamad.zolfaghari <you@example.com>
2021-10-01 22:59:03 +0330

    The App.tsx was updated.

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,14 @@ import Header from './components/UI/Layout/Header/Header';
 import Main from './components/Main/Main';
 
 function App() {
-    return (
-        <>
-            Redirect to UI {process.env.REACT_APP_REDIRECT_URI}
-            <Header/>
-            <main>
-                <Main/>
-            </main>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <main>
+        <Main />
+      </main>
+    </>
+  );
 }
 
 export default App;
